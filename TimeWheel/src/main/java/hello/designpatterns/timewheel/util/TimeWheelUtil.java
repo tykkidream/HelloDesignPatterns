436bb0dc75157d2f898470320acd48076aa8f8a9
Tykkidream <tykkidream@gmail.com>
2017-10-16 16:50:05 +0800

    添加时间轮

---
+++ TimeWheel/src/main/java/hello/designpatterns/timewheel/util/TimeWheelUtil.java
@@ -0,0 +1,10 @@
+package hello.designpatterns.timewheel.util;
+
+import hello.designpatterns.timewheel.TimeWheel;
+
+/**
+ * Created by Saber on 2017/4/16.
+ */
+public class TimeWheelUtil {
+    public final static TimeWheel TimeWheelNull = new TimeWheelNull();
+}
