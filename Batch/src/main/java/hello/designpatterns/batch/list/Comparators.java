b5595bb79ca94141f5070e63d572423de2b1d6cb
tykkidream <tykkidream@gmail.com>
2022-07-12 17:37:40 +0800

    添加  cascadeDiffComparator 接口

--- Batch/src/main/java/hello/designpatterns/batch/list/Comparators.java
+++ Batch/src/main/java/hello/designpatterns/batch/list/Comparators.java
@@ -14,4 +14,16 @@ public class Comparators {
             return 0;
         };
     }
+
+    public static <A, B> DiffComparator<A, B> cascadeDiffComparator(DiffComparator<A, B> ... cs) {
+        return (o1, o2) -> {
+            for (DiffComparator<A, B> c : cs) {
+                int compare = c.compare(o1, o2);
+                if (compare != 0) {
+                    return compare;
+                }
+            }
+            return 0;
+        };
+    }
 }
