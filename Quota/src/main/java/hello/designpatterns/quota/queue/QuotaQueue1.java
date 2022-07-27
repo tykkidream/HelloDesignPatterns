4b7ee710a39b76456cd94f49093798df7ed7f7c5
tykkidream <tykkidream@gmail.com>
2022-07-27 09:22:39 +0800

    初步实现 QuotaQueue1，有并发问题未解决

---
+++ Quota/src/main/java/hello/designpatterns/quota/queue/QuotaQueue1.java
@@ -0,0 +1,192 @@
+package hello.designpatterns.quota.queue;
+
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 未完成，有并发问题。
+ * @param <T>
+ */
+public class QuotaQueue1<T> {
+    private static final long FIRST_TIME_OUT = 60 * 1000;
+
+    private static <T extends Exception, R> R throwException(Throwable t) throws T {
+        throw (T) t;
+    }
+
+
+    private BlockingQueue<QuotaRule> taskQueues;
+
+    private List<QuotaRule> taskQueueRounds;
+
+    /**
+     * 所有业务的任务，在根据配额规则计算后，编排进入此队列。
+     */
+    private BlockingQueue<T> taskQuotaQueue;
+
+    private Map<String, QuotaRule> quotaRules;
+
+    private Thread quotaShapingThread;
+
+    public QuotaQueue1(String name) {
+        taskQueues = new LinkedBlockingDeque<>();
+
+        taskQueueRounds = new LinkedList<>();
+
+        taskQuotaQueue = new LinkedBlockingDeque<>();
+
+        quotaRules = new HashMap<>();
+
+        quotaShapingThread = new Thread(new QuotaShaping(), "QuotaQueue-Thread-" + name);
+
+        quotaShapingThread.start();
+    }
+
+    private class QuotaShaping implements Runnable {
+
+        @Override
+        public void run() {
+            while (!quotaShapingThread.isInterrupted()) {
+                try {
+                    QuotaQueue1.this.run();
+                } catch (Throwable throwable) {
+                    throwable.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public synchronized Queue<T> addQuota(String name, int quota) {
+        return addQuota(name, quota, null);
+    }
+
+    public synchronized Queue<T> addQuota(String name, int quota, BlockingQueue<T> queue) {
+        if (quotaRules.containsKey(name)) {
+            return null;
+        }
+
+        if (queue == null) {
+            queue = new ArrayBlockingQueue<>(quota);
+        }
+
+        QuotaRule quotaRule = new QuotaRule();
+        quotaRule.name = name;
+        quotaRule.quota = quota;
+        quotaRule.queue = queue;
+
+        quotaRules.put(name, quotaRule);
+
+        return queue;
+    }
+
+    /**
+     * 与 {@link #run()} 接口有并发问题。
+     * @param name
+     * @param element
+     * @throws InterruptedException
+     */
+    public void put(String name, T element) throws InterruptedException {
+        QuotaRule quotaRule = quotaRules.get(name);
+
+        int in = quotaRule.in.incrementAndGet();
+
+        if (in > 1) {
+            return;
+        }
+
+        synchronized (quotaRule) {
+            taskQueues.add(quotaRule);
+        }
+    }
+
+    /**
+     * 与 {@link #put(String, Object)} 接口有并发问题。
+     */
+    public void run() {
+        while (poll(FIRST_TIME_OUT, TimeUnit.SECONDS)) {
+            while (poll(0, null)) {}
+
+            if (taskQueueRounds.size() > 0) {
+                Iterator<QuotaRule> iterator = taskQueueRounds.iterator();
+
+                while (iterator.hasNext()) {
+                    QuotaRule quotaRule = iterator.next();
+                    taskQueues.add(quotaRule);
+                    iterator.remove();
+                }
+            } else {
+                break;
+            }
+        }
+    }
+
+    private boolean poll(long timeout, TimeUnit unit) {
+        try {
+            QuotaRule quotaRule = null;
+
+            if (timeout <= 0) {
+                quotaRule = taskQueues.poll();
+            } else {
+                    quotaRule = taskQueues.poll(timeout, unit);
+
+            }
+
+            if (quotaRule == null) {
+                return false;
+            }
+
+            if (quotaRule.queue.isEmpty()) {
+                int in = quotaRule.in.get();
+                synchronized (quotaRule) {
+                    if (quotaRule.queue.isEmpty()) {
+                        boolean set = quotaRule.in.compareAndSet(in, 0);
+                        if (set) {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < quotaRule.quota; i++) {
+                T task = quotaRule.queue.poll();
+
+                if (task != null) {
+                    taskQuotaQueue.put(task);
+                }
+            }
+
+            // 此时，quotaRule.quota 只可能有 0 个数据，也要将其添加到 taskQueueRounds 中一次。
+            // 下一次执行本接口 poll 时，将对本轮所有 taskQueues 中还未被处理的下一个或下 N 个 quotaRule 进行处理，
+            // 如果不将当前 quotaRule 添加到 taskQueueRounds 中，当 quotaRule.quota 突然有了新数据时，
+            // 可能导致当前 quotaRule 插队到本轮 taskQueues 后续还未被处理 quotaRule 之前，多执行处理一次，变得不公平。
+            taskQueueRounds.add(quotaRule);
+
+            quotaRule.in.set(2);
+
+
+            return true;
+        } catch (InterruptedException e) {
+            throwException(e);
+            return false;
+        }
+    }
+
+    public T poll() {
+        return taskQuotaQueue.poll();
+    }
+
+    private class QuotaRule {
+        private String name;
+
+        private int quota;
+
+        private BlockingQueue<T> queue;
+
+        private AtomicInteger in = new AtomicInteger(0);
+    }
+
+}
---
+++ Quota/src/test/java/hello/designpatterns/quota/queue/QuotaQueue1Test.java
@@ -0,0 +1,141 @@
+package hello.designpatterns.quota.queue;
+
+import org.junit.Test;
+
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class QuotaQueue1Test {
+
+    @Test
+    public void test1() throws InterruptedException {
+        QuotaQueue1<String> quotaQueue = new QuotaQueue1("abc");
+
+        Queue<String> queue1 = quotaQueue.addQuota("a", 1, new LinkedBlockingDeque<>());
+        Queue<String> queue2 = quotaQueue.addQuota("b", 2, new LinkedBlockingDeque<>());
+        Queue<String> queue3 = quotaQueue.addQuota("c", 1, new LinkedBlockingDeque<>());
+        Queue<String> queue4 = quotaQueue.addQuota("d", 3, new LinkedBlockingDeque<>());
+
+        queue1.add("a-quota1-0");
+        queue1.add("a-quota1-1");
+        queue1.add("a-quota1-2");
+        queue1.add("a-quota1-3");
+        queue1.add("a-quota1-4");
+        queue1.add("a-quota1-5");
+        queue1.add("a-quota1-6");
+        queue1.add("a-quota1-7");
+        queue1.add("a-quota1-8");
+        queue1.add("a-quota1-9");
+
+        queue2.add("b-quota2-0");
+        queue2.add("b-quota2-1");
+        queue2.add("b-quota2-2");
+        queue2.add("b-quota2-3");
+        queue2.add("b-quota2-4");
+        queue2.add("b-quota2-5");
+        queue2.add("b-quota2-6");
+        queue2.add("b-quota2-7");
+        queue2.add("b-quota2-8");
+        queue2.add("b-quota2-9");
+
+        queue3.add("c-quota1-0");
+        queue3.add("c-quota1-1");
+        queue3.add("c-quota1-2");
+        queue3.add("c-quota1-3");
+        queue3.add("c-quota1-4");
+        queue3.add("c-quota1-5");
+        queue3.add("c-quota1-6");
+        queue3.add("c-quota1-7");
+        queue3.add("c-quota1-8");
+        queue3.add("c-quota1-9");
+
+        queue4.add("d-quota3-0");
+        queue4.add("d-quota3-1");
+        queue4.add("d-quota3-2");
+        queue4.add("d-quota3-3");
+        queue4.add("d-quota3-4");
+        queue4.add("d-quota3-5");
+        queue4.add("d-quota3-6");
+        queue4.add("d-quota3-7");
+        queue4.add("d-quota3-8");
+        queue4.add("d-quota3-9");
+
+        quotaQueue.run();
+
+        while (true) {
+            String poll = quotaQueue.poll();
+
+            if (poll == null) {
+                break;
+            }
+
+            System.out.println(poll);
+        }
+    }
+
+    @Test
+    public void test2() throws InterruptedException {
+        QuotaQueue1<String> quotaQueue = new QuotaQueue1("abc");
+
+        Queue<String> queue1 = quotaQueue.addQuota("a", 1, new LinkedBlockingDeque<>());
+        Queue<String> queue2 = quotaQueue.addQuota("b", 2, new LinkedBlockingDeque<>());
+        Queue<String> queue3 = quotaQueue.addQuota("c", 1, new LinkedBlockingDeque<>());
+        Queue<String> queue4 = quotaQueue.addQuota("d", 3, new LinkedBlockingDeque<>());
+
+        quotaQueue.put("a", "a-quota1-0");
+        quotaQueue.put("a", "a-quota1-1");
+        quotaQueue.put("a", "a-quota1-2");
+        quotaQueue.put("a", "a-quota1-3");
+        quotaQueue.put("a", "a-quota1-4");
+        quotaQueue.put("a", "a-quota1-5");
+        quotaQueue.put("a", "a-quota1-6");
+        quotaQueue.put("a", "a-quota1-7");
+        quotaQueue.put("a", "a-quota1-8");
+        quotaQueue.put("a", "a-quota1-9");
+
+        quotaQueue.put("b", "b-quota2-0");
+        quotaQueue.put("b", "b-quota2-1");
+        quotaQueue.put("b", "b-quota2-2");
+        quotaQueue.put("b", "b-quota2-3");
+        quotaQueue.put("b", "b-quota2-4");
+        quotaQueue.put("b", "b-quota2-5");
+        quotaQueue.put("b", "b-quota2-6");
+        quotaQueue.put("b", "b-quota2-7");
+        quotaQueue.put("b", "b-quota2-8");
+        quotaQueue.put("b", "b-quota2-9");
+
+        quotaQueue.put("c", "c-quota1-0");
+        quotaQueue.put("c", "c-quota1-1");
+        quotaQueue.put("c", "c-quota1-2");
+        quotaQueue.put("c", "c-quota1-3");
+        quotaQueue.put("c", "c-quota1-4");
+        quotaQueue.put("c", "c-quota1-5");
+        quotaQueue.put("c", "c-quota1-6");
+        quotaQueue.put("c", "c-quota1-7");
+        quotaQueue.put("c", "c-quota1-8");
+        quotaQueue.put("c", "c-quota1-9");
+
+        quotaQueue.put("d", "d-quota3-0");
+        quotaQueue.put("d", "d-quota3-1");
+        quotaQueue.put("d", "d-quota3-2");
+        quotaQueue.put("d", "d-quota3-3");
+        quotaQueue.put("d", "d-quota3-4");
+        quotaQueue.put("d", "d-quota3-5");
+        quotaQueue.put("d", "d-quota3-6");
+        quotaQueue.put("d", "d-quota3-7");
+        quotaQueue.put("d", "d-quota3-8");
+        quotaQueue.put("d", "d-quota3-9");
+
+        Thread.sleep(300000);
+
+        while (true) {
+            String poll = quotaQueue.poll();
+
+            if (poll == null) {
+                break;
+            }
+
+            System.out.println(poll);
+        }
+    }
+}
