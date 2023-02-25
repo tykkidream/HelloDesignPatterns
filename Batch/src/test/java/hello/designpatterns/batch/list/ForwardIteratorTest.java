779b2fdd900a08dd4ed05461d5572313128d5963
tykkidream <tykkidream@gmail.com>
2023-02-25 11:14:15 +0800

    测试迭代器删除性能

--- Batch/src/test/java/hello/designpatterns/batch/list/ForwardIteratorTest.java
+++ Batch/src/test/java/hello/designpatterns/batch/list/ForwardIteratorTest.java
@@ -3,9 +3,7 @@ package hello.designpatterns.batch.list;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class ForwardIteratorTest {
 
@@ -19,6 +17,24 @@ public class ForwardIteratorTest {
         persons.add(new Person(2L, "tengxun"));
         persons.add(new Person(4L, "huawei"));
         persons.add(new Person(1L, "baidu"));
+    }
+
+    public List<Integer> dataArray(int total) {
+        List<Integer> list = new ArrayList<>(total);
+
+        for (int i = 0; i < total; i++) {
+            list.add(i);
+        }
+        return list;
+    }
+
+    public List<Integer> dataLinked(int total) {
+        List<Integer> list = new LinkedList<>();
+
+        for (int i = 0; i < total; i++) {
+            list.add(i);
+        }
+        return list;
     }
 
     @Test
@@ -155,5 +171,35 @@ public class ForwardIteratorTest {
                 System.out.println(iterator.next());
             }
         }
+    }
+
+    @Test
+    public void test7(){
+        List<Integer> dataArray = WrapList.wrap(dataArray(2000)) ;
+        List<Integer> dataLinked = WrapList.wrap(dataLinked(2000));
+//        List<Integer> dataArray = dataArray(2000) ;
+//        List<Integer> dataLinked = dataLinked(2000);
+
+        Iterator<Integer> iterator1 = dataArray.iterator();
+        Iterator<Integer> iterator2 = dataLinked.iterator();
+
+        long time1 = System.nanoTime();
+
+        while (iterator1.hasNext()) {
+            Integer next = iterator1.next();
+            iterator1.remove();
+        }
+
+        long time2 = System.nanoTime();
+
+        while (iterator2.hasNext()) {
+            Integer next = iterator2.next();
+            iterator2.remove();
+        }
+
+        long time3 = System.nanoTime();
+
+        System.out.println("第一次耗时： " + (time2 - time1));
+        System.out.println("第二次耗时： " + (time3 - time2));
     }
 }
